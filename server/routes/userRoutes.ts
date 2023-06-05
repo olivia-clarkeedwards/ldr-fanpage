83d3dc17ccbad1d7fe898e45b2c40c156fa5df05
Olivia <oliviaclarkeedwards@gmail.com>
2023-06-05 18:40:43 +1200

    add user type, import model

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,11 +1,12 @@
 import express from 'express'
-import { getUsers } from '../db/user-db'
+import { getUsers } from '../db/functions/user-db'
+import { User } from '../../common/User'
 
 const router = express.Router()
 
 router.get('/', (_, res) => {
   getUsers()
-    .then((users) => {
+    .then((users: User[]) => {
       if (users.length !== 0) res.json(users)
       else throw Error('No users found.')
     })
