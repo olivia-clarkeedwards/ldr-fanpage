c4894a46bce6323b66c7d491f98bc93528b3aa3f
Olivia <oliviaclarkeedwards@gmail.com>
2023-06-05 18:35:14 +1200

    tidy album functions

--- server/db/album-db.ts
+++ server/db/album-db.ts
@@ -1,19 +1,19 @@
 import config from './knexfile'
 import knex from 'knex'
-import { Album, AlbumWithSongs } from '../../common/Album'
+import { Album } from '../../common/Album'
 
 type Environment = 'production' | 'test' | 'development'
 const environment = (process.env.NODE_ENV as Environment) || 'development'
 const connection = knex(config[environment])
 
-/*
- * albums
+/*************************
+ ********* albums *********
  *************************/
-
-export async function getAlbums(db = connection): Promise<Album[]> {
-  return db('albums').select('*')
-}
 
 export function getAlbum(id: number, db = connection): Promise<Album> {
   return db('albums').select().where('id', id).first()
+}
+
+export async function getAlbums(db = connection): Promise<Album[]> {
+  return db('albums').select('*')
 }
