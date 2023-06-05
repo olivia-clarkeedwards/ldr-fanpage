774eb640cbef6177c0c116f5d697526ddf9cad76
Olivia <oliviaclarkeedwards@gmail.com>
2023-06-05 18:42:10 +1200

    add type

--- server/routes/albumRoutes.ts
+++ server/routes/albumRoutes.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import { getAlbumsWithSongs } from '../db/functions/dbUtils'
+import { AlbumWithSongs } from '../../common/Album'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', (_, res) => {
   getAlbumsWithSongs()
-    .then((albums) => {
+    .then((albums: AlbumWithSongs[]) => {
       if (albums.length !== 0) res.json(albums)
       else throw Error('No albums found.')
     })
